7fac6a395b1449cae115d99ceda1dd5dd9a3ed81
Aguscrack20 <aguspalaciotablet@gmail.com>
2026-06-18 15:50:12 -0300

    Cambio1

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
+#include "Pila/Pila.h"
 using namespace std;
+
+void palabrainvertidapila(Pila<char>&palabra){
+
+   char caracter;           //Al parecer Char es para guardar letras....
+
+   do
+   {
+    cin.get(caracter);
+    if (caracter != '.')
+    {
+        palabra.push(caracter);
+    }
+    
+   } while (caracter != '.');
+   
+
+   cout << "Palabra o frase invertida: ";
+
+    // Desapila cada caracter y lo imprime, invirtiendo así la palabra o frase
+    while (!palabra.esVacia()) {
+        cout << palabra.pop();
+    }
+
+    cout << endl;
+
+
+
+}
 
 int main() {
 
     cout << "Ejercicio N° 1" << endl;
+    Pila<char> pal;
+    char d;
+    cout<<"Ingrese palabra o frase la cual le gustaria invertir(Por favor usar punto final)"<<endl;
+    cin>>d;
+
+    pal.push(d);
+
+    palabrainvertidapila(pal);
 
     return 0;
 }
